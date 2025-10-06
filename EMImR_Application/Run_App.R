bcb12de78765a285d5b7362aa7d897eca6555810
Hiba Ben Aribi <73958439+HibaBenAribi@users.noreply.github.com>
2025-10-06 22:56:29 +0100

    Update Run_App.R

--- EMImR_Application/Run_App.R
+++ EMImR_Application/Run_App.R
@@ -1,9 +1,17 @@
-## Run the following  code in order
+species_list <- c(
+  "Homo sapiens",
+  "Mus musculus",
+  "Arabidopsis thaliana",
+  "Drosophila melanogaster",
+  "Danio rerio",
+  "Rattus norvegicus",
+  "Saccharomyces cerevisiae",
+  "Caenorhabditis elegans"
+)
 source("Dependencies_emimr.R")
 source("Ui.R",local = TRUE)
 
 source("Server.R",local = TRUE)
 
 # Run the application
-
 shinyApp(ui = Ui, server = Server)
